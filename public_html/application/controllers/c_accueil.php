3bb7230a9b7e1857b7efa2a72919f68f835a81a0
b34 <b34@btsiris.local>
2019-01-24 17:02:54 +0100

    Debut login

--- public_html/application/controllers/c_accueil.php
+++ public_html/application/controllers/c_accueil.php
@@ -1,14 +1,26 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Controlleur de la page principale du site.
+
 class c_accueil extends CI_Controller {
-    
-    public function __construct(){
+
+    public function __construct() {
         parent::__construct();
         $this->load->helper('url');
     }
-    
-    public function index(){
-    $this->load->view('v_accueil');
+
+    public function index() {
+        // Verification de la validitee de la session
+        if ($this->session->userdata('logged_in')) {
+            
+            $session_data = $this->session->userdata('logged_in');
+            $data['username'] = $session_data['username'];
+            $this->load->view('v_accueil', $data);  // --> Verif OUI
+        } else {
+            redirect('v_login', 'refresh');         // --> Verif NON   
+        }
     }
+
 }
