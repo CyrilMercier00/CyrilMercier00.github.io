ba0c6b7356b58b0142c4b6da98bce192e7602e57
b34 <b34@btsiris.local>
2019-03-18 12:04:01 +0100

    affichge accueil

--- public_html/application/views/v_accueil.php
+++ public_html/application/views/v_accueil.php
@@ -1,76 +1,96 @@
-<div class="container-fluid">
-    <div class="row">
-    </div>
-
-    <div class="container-list col-lg-6" id="divliste">
-        <div class="au-card-inner ">
-            <ul class="list-group" id="liste_machines">
-            </ul>
+<div class="container-fluid center">
+    
+        <!-- DEBUT liste de machines -->
+        <div class="container-list col-lg-4" id="divliste">
+            <div class="au-card-inner">
+                <div class="list_heading">Liste des machines</div>
+                <ul class="list-group" id="liste_machines">
+                </ul>
+            </div>
         </div>
-    </div>
+        <!--FIN liste des machines -->
 
+        <!-- DEBUT liste des evenements -->
+        <div class="">
+            <div class="col-lg-8 pull-right">
+                <div class="au-card au-card--no-shadow au-card--no-pad m-b-40 ">
 
-    <div class="row">
-        <div class="col-lg-8">
-            <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
-                <div class="au-card-title" style="">
-                    <div class="bg-overlay bg-overlay--blue"></div>
+                    <!-- Titre -->
+                    <div class="au-card-title" style="">
+                        <div class="bg-overlay bg-overlay--blue"></div>
+                        <h3> <i class="zmdi zmdi-account-calendar"></i>Journal d'évènement</h3>
+                    </div>
 
-                    <h3> <i class="zmdi zmdi-account-calendar"></i>Journal d'évènement</h3>
-                    <button class="au-btn-plus">
-                        <i class="zmdi zmdi-plus"></i>
-                    </button>
-                </div>
-                <div class="au-task js-list-load">
-                    <div class="au-task-list js-scrollbar3">
-                        <div class="au-task__item au-task__item--danger">
-                            <div class="au-task__item-inner">
-                                <h5 class="task">
-                                    <a href="#">Exemple évènement très important</a>
-                                </h5>
-                                <span class="time">10:00</span>
-                            </div>
-                        </div>
-                        <div class="au-task__item au-task__item--warning">
-                            <div class="au-task__item-inner">
-                                <h5 class="task">
-                                    <a href="#">Exemple évènement important</a>
-                                </h5>
-                                <span class="time">11:00</span>
-                            </div>
-                        </div>
-                        <div class="au-task__item au-task__item--success">
-                            <div class="au-task__item-inner">
-                                <h5 class="task">
-                                    <a href="#">Exemple évènement</a>
-                                </h5>
-                                <span class="time">03:30</span>
-                            </div>
-                        </div>
-                        <div class="au-task__item au-task__item--danger js-load-item">
-                            <div class="au-task__item-inner">
-                                <h5 class="task">
-                                    <a href="#">Evenement supplémentaire</a>
-                                </h5>
-                                <span class="time">10:00 AM</span>
-                            </div>
-                        </div>
-                        <div class="au-task__item au-task__item--warning js-load-item">
-                            <div class="au-task__item-inner">
-                                <h5 class="task">
-                                    <a href="#">Evenement supplémentaire</a>
-                                </h5>
-                                <span class="time">11:00 AM</span>
-                            </div>
+                    <div class="au-task js-list-load">
+                        <div class="au-task-list js-scrollbar3">
+
+                            <!-- contenu de la liste des evenements -->
+                            <ul>
+
+                                <li>  
+                                    <div class="au-task__item au-task__item--danger">
+                                        <div class="au-task__item-inner">
+                                            <h5 class="task">
+                                                <a href="#"> DANGER - La fraiseuse n°3 est passée dans le seuil D</a>
+                                            </h5>
+                                            <span class="time">10:32</span>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="au-task__item au-task__item--warning">
+                                        <div class="au-task__item-inner">
+                                            <h5 class="task">
+                                                <a href="#"> ATTENTION - La tours a métaux n°1 est passée dans le seuil C</a>
+                                            </h5>
+                                            <span class="time">11:21</span>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="au-task__item au-task__item--success">
+                                        <div class="au-task__item-inner">
+                                            <h5 class="task">
+                                                <a href="#">ATTENTION - La fraiseuse n°1 est passée dans le seuil B</a>
+                                            </h5>
+                                            <span class="time">19:26</span>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="au-task__item au-task__item--success">
+                                        <div class="au-task__item-inner">
+                                            <h5 class="task">
+                                                <a href="#">Exemple évènement</a>
+                                            </h5>
+                                            <span class="time">03:30</span>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="au-task__item au-task__item--success">
+                                        <div class="au-task__item-inner">
+                                            <h5 class="task">
+                                                <a href="#">Exemple évènement</a>
+                                            </h5>
+                                            <span class="time">16:35</span>
+                                        </div>
+                                    </div>
+                                </li>
+
+                            </ul>
+
                         </div>
                     </div>
-                    <div class="au-task__footer">
-                        <button class="au-btn au-btn-load js-load-btn">load more</button>
-                    </div>
+
                 </div>
             </div>
         </div>
-    </div>
-
-    
+        <!-- FIN liste des evenements -->
+        
 </div>
+<!-- FIN page
