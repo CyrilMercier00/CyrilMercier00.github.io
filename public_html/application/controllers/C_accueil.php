ce70e7bea6d690e8243d370eea16c58dd2760bb9
equilix <equilix@outlook.fr>
2019-12-17 19:52:05 +0100

    disable login

--- public_html/application/controllers/C_accueil.php
+++ public_html/application/controllers/C_accueil.php
@@ -9,17 +9,20 @@ class C_accueil extends CI_Controller {
     }
 
     public function index() {
+        // Github only
+        $this->session->userdata('logged_in') == true
+
         // Verification de la validitee de la session
         if (($this->session->userdata('logged_in') == true)) {
 
             $page = $this->load->view('V_accueil','',true);
-            
+
             $data = array(
                 'page' => $page,
                 'css_location' => 'v_accueil.css',
                 'javascript_location' => 'c_accueil.js'
             );
-            
+
             $this->load->view('common/v_template', array('data' => $data), false);
         } else {
             redirect('C_login');
